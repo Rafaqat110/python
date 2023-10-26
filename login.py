ada44009df1a26060c4b100025bdc51c705da3fb
Rafaqat110 <140418576+Rafaqat110@users.noreply.github.com>
2023-10-26 20:27:54 +0500

    F21BSEEN1E02005 - LoginPage

--- login.py
+++ login.py
@@ -1,17 +1,33 @@
 from tkinter import *
+from tkinter import messagebox
 import os
+
+def submit():
+	global screen3
+	screen3 = Toplevel(screen)
+	screen3.geometry("700x400")
+	screen3.title("user List")
+	username_info1 = username_verify.get()
+	password_info1 = password_verify.get()
+	Label(text="",height= '3',width='30').pack()
+	# Label(screen3,text="Wellcome "+username_info1,font ="green",bg="light salmon",height= '2',width='30').pack()
+	Label(screen3,text="Wellcome !  "+username_info1,bg="#c0ecc0", fg="black",
+          width="300", height="2",font=("Calibri", 13)).pack(padx=20, pady=23 )
+	Label(screen3,text="",height= '10',width='30').pack()
+	Label(screen3,text="UserName :"+username_info1,font ="green",height= '2',width='30').pack()
+	Label(screen3,text="Password :"+password_info1,font ="green",height= '2',width='30').pack()
+	
+
 
 
 def register_user():
 	username_info = username.get()
 	password_info = password.get()
-	
-	
 	file = open(username_info,"w")
 	file.write(username_info+"\n")
 	file.write(password_info)
 	file.close()
-	Label(screen1,text= username_info,font ="green",height= '2',width='30').pack()
+	Label(screen1,text= "Account Created Successfully...!",font ="green",height= '2',width='30').pack()
 
 
 def register():
@@ -25,7 +41,9 @@ def register():
 	global password_entry
 	username = StringVar()
 	password = StringVar()
-	Label(screen1,text="  Please Enter The Given Information  ",height ="2", width="30")
+	Label(screen1,text="Enter Details Below to Sign Up!",bg="#c0ecc0", fg="black",
+          width="300", height="2",font=("Calibri", 13)).pack(padx=20, pady=23 )
+	# Label(screen1,text="  Please Enter The Given Information  ",height ="2", width="30")
 	Label(screen1,text="",height= '2',width='30').pack()
 	Label(screen1,text="Username * ",height= '2',width='30').pack()
 	username_entry= Entry(screen1,textvariable = username)
@@ -45,10 +63,13 @@ def login_verify():
 		verify = file.read().splitlines()
 		if password1 in verify:
 			print ("Success")
+			submit()
 		else:
+			messagebox.showerror("error","wrong password")
 			print ("Wrong Password ")
 
 	else :
+		messagebox.showerror("error","User not found")
 		print ("User not found ")
 
 
@@ -62,7 +83,9 @@ def login():
 	screen2 = Toplevel(screen)
 	screen2.geometry("700x400")
 	screen2.title("login")
-	Label(screen2,text="Please enter the information for login ",height= '2',width='30').pack()
+	Label(screen2,text="Please enter the information for login!",bg="#f2d0d7", fg="black",
+          width="300", height="2",font=("Calibri", 13)).pack(padx=20, pady=23 )
+	# Label(screen2,text="Please enter the information for login ",height= '2',width='30').pack()
 	Label(screen2,text="",height= '2',width='30').pack()
 	Label(screen2,text="Username : ",height= '2',width='30').pack()
 	username_entry1 = Entry(screen2,textvariable = username_verify)
@@ -71,17 +94,18 @@ def login():
 	password_entry1 = Entry (screen2,textvariable = password_verify)
 	password_entry1.pack()
 	Label(screen2,text="",height= '2',width='30').pack()
-	Button(screen2, text="login", height= "2",width="30",bg="spring green", command = login_verify).pack()
+	Button(screen2, text="login", height= "2",width="30",bg="spring green", command =login_verify).pack()
 
 
 def main_screen():
 	global screen
 	screen = Tk()
 	screen.geometry("700x400")
-	Label(text="Login GUI Interface").pack()
-	Label(text="",height= '3',width='30')
+	Label(text="",height= '2',width='30').pack()
+	Label(text="Login GUI Interface",font=SOLID).pack()
+	Label(text="",height= '5',width='30').pack()
 	Button(text="Login ",height= '2',width='30',bg="spring green",command = login).pack()
-	Label(text="",height= '3',width='30').pack()
+	Label(text="",height= '2',width='30').pack()
 	Button(text="Register",height= '2',width='30',bg="light salmon", command= register).pack()
 	screen.mainloop()
 
